f63dfbf7780b92b248d2eb6c441988821a293c68
Parag Jain <ga53xim@mytum.de>
2018-08-20 14:54:41 +0200

    drop down menu and action on selection

---
+++ script.js
@@ -0,0 +1,12 @@
+function MarbleClicked()
+{
+    document.getElementById('stair').style.backgroundImage = "url(Images/Marble.jpg)";
+}
+
+function ItalianClicked() {
+    document.getElementById('stair').style.backgroundImage = "url(Images/ItalianMarble.jpg)";
+}
+
+
+
+    
