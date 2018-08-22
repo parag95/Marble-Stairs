1a09cbbc5bf9c77707a9c00fd098d0c98fe9d88f
Parag Jain <ga53xim@mytum.de>
2018-08-22 15:15:54 +0200

    improved the stairs

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ function btn1MarbleClicked()
     document.getElementById('stair2-front').style.backgroundImage = "url(Images/Marble.jpg)";
 }
 
-function btn1ItalianClicked() 
+function btn1ItalianMarbleClicked() 
 {
     document.getElementById('stair1-front').style.backgroundImage = "url(Images/ItalianMarble.jpg)";
     document.getElementById('stair2-front').style.backgroundImage = "url(Images/ItalianMarble.jpg)";
 }
 
+function btn1GreenMarbleClicked() {
+    document.getElementById('stair1-top').style.backgroundImage = "url(Images/GreenMarble.jpg)";
+    document.getElementById('stair2-top').style.backgroundImage = "url(Images/GreenMarble.jpg)";
+}
 
 function btn2MarbleClicked()
 {
@@ -17,8 +21,12 @@ function btn2MarbleClicked()
     document.getElementById('stair2-top').style.backgroundImage = "url(Images/Marble.jpg)";
 }
 
-function btn2ItalianClicked() {
+function btn2ItalianMarbleClicked() {
     document.getElementById('stair1-top').style.backgroundImage = "url(Images/ItalianMarble.jpg)";
     document.getElementById('stair2-top').style.backgroundImage = "url(Images/ItalianMarble.jpg)";
 }
-    
+ 
+function btn2GreenMarbleClicked() {
+    document.getElementById('stair1-top').style.backgroundImage = "url(Images/GreenMarble.jpg)";
+    document.getElementById('stair2-top').style.backgroundImage = "url(Images/GreenMarble.jpg)";
+}
