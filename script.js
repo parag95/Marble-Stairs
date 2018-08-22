07d37ef4200308186fbb0874ed1d54d8739ff551
Parag Jain <ga53xim@mytum.de>
2018-08-22 14:02:16 +0200

    first version of page

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-function MarbleClicked()
+function btn1MarbleClicked()
 {
-    document.getElementById('stair').style.backgroundImage = "url(Images/Marble.jpg)";
+    document.getElementById('stair1-front').style.backgroundImage = "url(Images/Marble.jpg)";
+    document.getElementById('stair2-front').style.backgroundImage = "url(Images/Marble.jpg)";
 }
 
-function ItalianClicked() {
-    document.getElementById('stair').style.backgroundImage = "url(Images/ItalianMarble.jpg)";
+function btn1ItalianClicked() 
+{
+    document.getElementById('stair1-front').style.backgroundImage = "url(Images/ItalianMarble.jpg)";
+    document.getElementById('stair2-front').style.backgroundImage = "url(Images/ItalianMarble.jpg)";
 }
 
 
+function btn2MarbleClicked()
+{
+    document.getElementById('stair1-top').style.backgroundImage = "url(Images/Marble.jpg)";
+    document.getElementById('stair2-top').style.backgroundImage = "url(Images/Marble.jpg)";
+}
 
+function btn2ItalianClicked() {
+    document.getElementById('stair1-top').style.backgroundImage = "url(Images/ItalianMarble.jpg)";
+    document.getElementById('stair2-top').style.backgroundImage = "url(Images/ItalianMarble.jpg)";
+}
     
