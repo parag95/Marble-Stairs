9fe8de1b36fd859f06e5525b016f1b44511064c6
Parag Jain <ga53xim@mytum.de>
2018-08-27 09:44:56 +0200

    re-arranged stuff, improved look

--- script.js
+++ script.js
@@ -2,31 +2,38 @@ function btn1MarbleClicked()
 {
     document.getElementById('stair1-front').style.backgroundImage = "url(Images/Marble.jpg)";
     document.getElementById('stair2-front').style.backgroundImage = "url(Images/Marble.jpg)";
+    document.getElementById('stair3-front').style.backgroundImage = "url(Images/Marble.jpg)";
 }
 
 function btn1ItalianMarbleClicked() 
 {
     document.getElementById('stair1-front').style.backgroundImage = "url(Images/ItalianMarble.jpg)";
     document.getElementById('stair2-front').style.backgroundImage = "url(Images/ItalianMarble.jpg)";
+    document.getElementById('stair3-front').style.backgroundImage = "url(Images/ItalianMarble.jpg)";
 }
 
 function btn1GreenMarbleClicked() {
     document.getElementById('stair1-top').style.backgroundImage = "url(Images/GreenMarble.jpg)";
     document.getElementById('stair2-top').style.backgroundImage = "url(Images/GreenMarble.jpg)";
+    document.getElementById('stair3-top').style.backgroundImage = "url(Images/GreenMarble.jpg)";
 }
 
 function btn2MarbleClicked()
 {
     document.getElementById('stair1-top').style.backgroundImage = "url(Images/Marble.jpg)";
     document.getElementById('stair2-top').style.backgroundImage = "url(Images/Marble.jpg)";
+    document.getElementById('stair3-top').style.backgroundImage = "url(Images/Marble.jpg)";
 }
 
 function btn2ItalianMarbleClicked() {
     document.getElementById('stair1-top').style.backgroundImage = "url(Images/ItalianMarble.jpg)";
     document.getElementById('stair2-top').style.backgroundImage = "url(Images/ItalianMarble.jpg)";
+    document.getElementById('stair3-top').style.backgroundImage = "url(Images/ItalianMarble.jpg)";
 }
  
 function btn2GreenMarbleClicked() {
     document.getElementById('stair1-top').style.backgroundImage = "url(Images/GreenMarble.jpg)";
     document.getElementById('stair2-top').style.backgroundImage = "url(Images/GreenMarble.jpg)";
+    document.getElementById('stair3-top').style.backgroundImage = "url(Images/GreenMarble.jpg)";
 }
+
